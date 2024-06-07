21ccc9228e792bf9abab993ab412941b4e0f87b2
Yehia <Yehia>
2024-06-07 15:56:03 +0300

    Create Project Stracture

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react"
+import Header from "./components/header/header.jsx";
+import PlaySheet from "./components/playSheet/playSheet.jsx";
+
+function App() {
+
+
+  return (
+    <Fragment>
+      <Header />
+      <PlaySheet />
+    </Fragment>
+  )
+}
+
+export default App
