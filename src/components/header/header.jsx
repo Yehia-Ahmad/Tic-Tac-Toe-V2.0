5351d335d273def00422b24a7e5b4d552debddd5
Yehia <Yehia>
2024-06-07 15:56:54 +0300

    Add Header Component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.module.css";
+
+const Header = () => {
+    return (
+        <header>
+            <img src="game-logo.png" alt="tic-tac-toe" />
+            <h1>Tic-Tac-Toe</h1>
+        </header>
+    )
+};
+
+export default Header;
